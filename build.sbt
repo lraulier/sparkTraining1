6e24835c6cb114e294773e4b4e62ddfaf5a514bd
lraulier <lraulier@synotis.com>
2015-11-11 13:10:52 +0100

    init spark test project

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "first test"
+
+version := "0.1"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
+
+mainClass in (Compile,run) := Some("org.kzone.sparkTraining1.test1")
+
---
+++ src/main/scala/org/kzone/sparkTraining1/test1.scala
@@ -0,0 +1,27 @@
+package org.kzone.sparkTraining1
+
+import scala.math.random
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by kubu-kzone on 11/11/15.
+  */
+object test1 {
+  def main (args: Array[String]) {
+    val conf = new SparkConf().setAppName("test1")
+    .setMaster("local[2]")
+
+    val spark = new SparkContext(conf)
+    val slices = if (args.length > 0) args(0).toInt else 2
+    val n = 100000 * slices
+    val count = spark.parallelize(1 to n, slices).map { i =>
+      val x = random * 2 - 1
+      val y = random * 2 - 1
+      if (x*x + y*y < 1) 1 else 0
+    }.reduce(_ + _)
+    println("DANS LE DELUGE DES MESSAGES D'INFOS LORS DE L'EXECUTION " +
+      "DE NOTRE APPLICATION IMPOSSIBLE DE RETROUVER LE PETIT MESSAGE QUI SUIT ALORS AJOUTONS-EN UN MAXIMUM EN MAJUSCULE AVANT :)")
+    println("Pi est " + 4.0 * count / n)
+    spark.stop()
+  }
+}
